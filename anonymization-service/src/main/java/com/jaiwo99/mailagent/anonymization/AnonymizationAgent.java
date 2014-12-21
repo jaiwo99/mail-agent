da6c2cd51d9859e0fa31128445b6ccc17fb1f066
Liang Shi <jaiwo99@gmail.com>
2014-12-22 00:10:10 +0100

    #1
    kick off

---
+++ anonymization-service/src/main/java/com/jaiwo99/mailagent/anonymization/AnonymizationAgent.java
@@ -0,0 +1,16 @@
+package com.jaiwo99.mailagent.anonymization;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jaiwo99
+ */
+//@SpringBootApplication
+public class AnonymizationAgent {
+
+
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
---
+++ common-model/src/main/java/com/jaiwo99/mailagent/common/model/AnonymizedMail.java
@@ -0,0 +1,7 @@
+package com.jaiwo99.mailagent.common.model;
+
+/**
+ * @author jaiwo99
+ */
+public class AnonymizedMail {
+}
